ea2cfd90e4b5e25312c037369098f96b9af0240f
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-11 20:36:15 +0100

    adding Todo model

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+Uuid uuid = const Uuid();
+
+enum Filter{
+  all,
+  completed,
+  active,
+}
+
+
+class Todo extends Equatable {
+  final String id;
+  final String description;
+  final bool isCompleted;
+
+  Todo({
+    String? id,
+    required this.description,
+    this.isCompleted = false,
+  }) : id = id ?? uuid.v4();
+
+  @override
+  List<Object?> get props => [id, description, isCompleted];
+
+  @override
+  bool? get stringify => true;
+}

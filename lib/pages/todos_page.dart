24b5b271da7f4790e4ff54a9a88667af84c7348c
Khalid-AC <khalid.aici.ac@gmail.com>
2022-11-14 08:25:12 +0100

    adding searching and filtering

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_app/models/todo_model.dart';
 import 'package:todo_app/providers/active_todo_count.dart';
+import 'package:todo_app/providers/providers.dart';
 
 class TodosPage extends StatefulWidget {
   const TodosPage({Key? key}) : super(key: key);
@@ -19,7 +21,11 @@ class _TodosPageState extends State<TodosPage> {
             padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
             child: Column(
-              children: [],
+              children: const [
+                TodoHeader(),
+                CreateTodo(),
+                SearchAndFilterTodo(),
+              ],
             ),
           ),
         ),
@@ -47,4 +53,90 @@ class TodoHeader extends StatelessWidget {
       ],
     );
   }
+}
+
+class CreateTodo extends StatefulWidget {
+  const CreateTodo({Key? key}) : super(key: key);
+
+  @override
+  State<CreateTodo> createState() => _CreateTodoState();
+}
+
+class _CreateTodoState extends State<CreateTodo> {
+  //
+  final newTodoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: newTodoController,
+      decoration: const InputDecoration(labelText: "What to do ?"),
+      onSubmitted: (String? todoDescription) {
+        if (todoDescription != null && todoDescription.trim().isNotEmpty) {
+          context.read<TodoList>().addTodo(todoDescription);
+          newTodoController.clear();
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    newTodoController.dispose();
+    super.dispose();
+  }
+}
+
+class SearchAndFilterTodo extends StatelessWidget {
+  const SearchAndFilterTodo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //
+      children: [
+        TextField(
+          decoration: const InputDecoration(
+            labelText: "Search Todos",
+            border: InputBorder.none,
+            filled: true,
+            prefixIcon: Icon(Icons.search),
+          ),
+          onChanged: (String? newSearchTerm) {
+            if (newSearchTerm != null) {
+              context.read<TodoSearch>().setSearchTerm(newSearchTerm);
+            }
+          },
+        ),
+        const SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            filterButton(context, Filter.all),
+            filterButton(context, Filter.completed),
+            filterButton(context, Filter.active),
+
+          ],
+        )
+      ],
+    );
+  }
+}
+
+Widget filterButton(BuildContext context, Filter filter) {
+  return TextButton(
+      onPressed: () {},
+      child: Text(
+        filter == Filter.all
+            ? "All"
+            : filter == Filter.active
+                ? "Active"
+                : "Completed",
+        style: TextStyle(fontSize: 18.0, color: textColor(context, filter)),
+      ));
+}
+
+Color textColor(BuildContext context, Filter filter) {
+  final currentFilter = context.watch<TodoFilter>().state.filter;
+  return currentFilter == filter ? Colors.blue : Colors.grey;
 }
